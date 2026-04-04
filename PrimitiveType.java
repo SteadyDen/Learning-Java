5776e3695a0d355eb41078ccd554b5b33b01e143
Den <forcodstud@gmail.com>
2026-04-04 22:13:11 +0900

    completed 3rd lecture

---
+++ PrimitiveType.java
@@ -0,0 +1,20 @@
+public class PrimitiveType {
+    public static void main(String[] args) {
+        
+        // 정수형
+        // 1. byte (1 byte = 8 bits) 
+        // 2^8 -> -2^7 ~ 2^7-1 (-128 ~ 127)
+
+        // 2. short (2 bytes = 16 bits)
+        // 2^16 -> -2^15 ~ 2^15-1 (-32768 ~ 32767)
+
+        // 3. int (4 bytes = 32 bits)
+        // 2^32 -> -2^31 ~ 2^31-1 
+
+        // 4. long (8 bytes = 64 bits)
+        // 2^64 -> -2^63 ~ 2^63-1 
+
+        
+    }
+    
+}
