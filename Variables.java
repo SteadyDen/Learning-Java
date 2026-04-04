99b63abeb74382911c2c3dcb228e0682e9094ec6
Den <forcodstud@gmail.com>
2026-04-04 21:53:36 +0900

    completed 2nd lecture

---
+++ Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        // 변수 => 데이터가 저장되는 공간
+        // 1. 1개의 변수 => 1개의 값만 할당 가능
+        // 2. 변수에 저장된 값 => 재할당을 통해 변경 가능
+        // 3. 값의 형태에 맞는 자료형을 사용
+        // 4. 변수명은 영어 소문자로 시작
+        // 5. 대소문자 구분, 공백 포함 불가능
+        // 6. 단어 두개 이어서 변수명 만들때는 첫글짜 대문자로
+        // 7. 자바 예약어로는 변수 사용 불가
+
+
+        int num = 1;
+        double width = 12.34;
+        String content = "Programming";
+
+        num = 10;
+        System.out.println(num);
+
+        int theExample = 10;
+    }
+}
