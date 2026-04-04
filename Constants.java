09ae586bbed2169b4bd4f2fdbf97c1926d351bcd
Den <forcodstud@gmail.com>
2026-04-04 23:31:33 +0900

    completed 8th lec

---
+++ Constants.java
@@ -0,0 +1,12 @@
+public class Constants {
+    public static void main(String[] args) {
+        
+        // 상수 (constants, final variables)
+        // 한번 값이 할당 => 재할당이 불가능
+        // 전체를 대문자, 단어간 구분은 언더바
+
+        final double PI_VALUE = 3.14;
+        System.out.println(PI_VALUE);
+        
+    }
+}
