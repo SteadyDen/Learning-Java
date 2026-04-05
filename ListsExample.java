04db513db38232cde389b30202f85ca4522b785c
Den <forcodstud@gmail.com>
2026-04-06 00:08:49 +0900

    completed list lec

---
+++ ListsExample.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ListsExample {
+    public static void main(String[] args) {
+        
+        // Lists
+        // 순서 구분, 중복 허용
+        // Vector, ArrayList, LinkedList
+
+        ArrayList<String> list = new ArrayList(10);
+        //list.add(100);
+        list.add("asdf");
+        
+        for (int i = 0; i<list.size(); ++i) {
+            System.out.println(list.get(i));
+        }
+    }
+}
