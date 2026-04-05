bfc416a457e85de67af378885e79f55d3454e156
Den <forcodstud@gmail.com>
2026-04-06 00:00:00 +0900

    completed Arrays lec

---
+++ ArraysExample.java
@@ -0,0 +1,19 @@
+public class ArraysExample {
+    public static void main(String[] args) {
+        
+        // 배열 (arrays)
+        
+        int[] test = {132, 321, 534, 654};
+        String[] test2 = {"1"};
+
+        System.out.println(test[0]);
+
+        System.out.println(test); // 배열은 참조 자료형이기 때문에 출력시 주소값이 나옴
+
+        System.out.println(test.length);
+
+        for (int i = 0; i < test.length; ++i) {
+            System.out.println(test[i]);
+        }
+    }
+}
